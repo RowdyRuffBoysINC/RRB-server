08c705959aaeccc47c497f15f1900a180ee96b43
Brent Guistwite <brentguistwite@gmail.com>
2018-04-03 13:04:53 -0400

    clean up console logs

--- test/socket.test.js
+++ test/socket.test.js
@@ -1,17 +1,17 @@
 import ioClient from 'socket.io-client';
 import io from 'socket.io';
 const should = require('chai').should();
-// const { app, runServer } = require('../server');
+// Const { app, runServer } = require('../server');
 
 const socketURL = 'http://localhost:8080/';
 
-const options ={
-  transports: [ 'websocket' , ],
+const options = {
+  transports: [ 'websocket', ],
   'force new connection': true,
 };
 
 const user1 = 'Brent';
-const user2 = {user: 'Vernon',};
+const user2 = { user: 'Vernon', };
 const room1 = 'rrb';
 
 describe('Server', () => {
@@ -20,7 +20,7 @@ describe('Server', () => {
     let client;
     beforeEach(() => {
       server = io().listen(8080);
-      // client = ioClient.connect(socketURL, options);
+      // Client = ioClient.connect(socketURL, options);
     });
     afterEach(() => {
       server.close();
@@ -29,7 +29,7 @@ describe('Server', () => {
 
     describe('room join', () => {
       it('Should allow users to join room', () => {
-        const roomData = {room: 'rrb', user: 'Brent',};
+        const roomData = { room: 'rrb', user: 'Brent', };
         server.on('connection', (socket) => {
           console.log('hello');
         });
@@ -38,35 +38,35 @@ describe('Server', () => {
     });
   });
 
-  
-  // it('Should allow users to join room', () => {
+
+  // It('Should allow users to join room', () => {
   //   // console.log('hello');
   //   // server.on('connection', (socket) => {
-  //     const socket = ioClient(socketURL);
-  //     socket.emit('join room', {room: room1, hiff: user1,});
+  //     Const socket = ioClient(socketURL);
+  //     Socket.emit('join room', {room: room1, hiff: user1,});
 
-  //     console.log('hi');
-  //     socket.on('join room', (data) => {
-  //       console.log(data);
-  //       data.should.be.type('object');
-  //       data.should.include.keys('room', 'user');
-  //       data.room.should.be.type('string');
-  //       data.user.should.be.type('string');
-  //       socket.join(data.room);
+  //     Console.log('hi');
+  //     Socket.on('join room', (data) => {
+  //       Console.log(data);
+  //       Data.should.be.type('object');
+  //       Data.should.include.keys('room', 'user');
+  //       Data.room.should.be.type('string');
+  //       Data.user.should.be.type('string');
+  //       Socket.join(data.room);
   //     });
-  //     socket.on('leave room', (data) => {
-  //       data.should.be.type('object');
-  //       data.should.include.keys('room', 'user');
-  //       data.room.should.be.type('string');
-  //       data.user.should.be.type('string');
-  //       socket.leave(data.room);
+  //     Socket.on('leave room', (data) => {
+  //       Data.should.be.type('object');
+  //       Data.should.include.keys('room', 'user');
+  //       Data.room.should.be.type('string');
+  //       Data.user.should.be.type('string');
+  //       Socket.leave(data.room);
   //     });
-  //     socket.on('code msg', (data) => {
-  //       socket.to(data.room).emit('code msg sent back to clients', data.msg);
+  //     Socket.on('code msg', (data) => {
+  //       Socket.to(data.room).emit('code msg sent back to clients', data.msg);
   //     });
-    // });
-    // client.disconnect();
-    // Done();
+  // });
+  // Client.disconnect();
+  // Done();
 
 
   // Socket.on('join room', (data) => {
