492ebdee99c98a16d0a8ef9e4140857124bcea9d
Vernon Mensah <vernonmensah@gmail.com>
2018-04-03 18:56:48 -0400

    no default secret

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/jwt-auth';
 export const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/jwt-auth';
 export const PORT = process.env.PORT || 8080;
-export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_SECRET = process.env.JWT_SECRET || 'hello';
 export const JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
